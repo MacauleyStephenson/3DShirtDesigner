ebd5834e3b5fb6bd867ed2be03a25c2a02363deb
MacauleyStephenson <MacauleyStephenson@mail.com>
2023-05-06 17:19:58 +0100

    adding key functionality for the item tabs

--- client/src/pages/Customiser.jsx
+++ client/src/pages/Customiser.jsx
@@ -7,13 +7,44 @@ import state from '../store';
 import { download } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
-import { fadeAnimation, slideAnimation } from '../config/animations';
+import { fadeAnimation, slideAnimation } from '../config/motion';
 
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
-function Customiser() {
+const Customiser = () => {
+	const snap = useSnapshot(state);
+
 	return (
-		<div>Customiser</div>
+		<AnimatePresence>
+			{!snap.intro && (
+				<>
+					<motion.div
+						key="custom"
+						className='absolute top-0 left-0 z-10'
+						{...slideAnimation('left')}
+					>
+						<div className='flex items-center min-h-screen'>
+							<div className='editortabs-container tabls'>
+								{EditorTabs.map((tab) => (
+									<Tab
+										key={tab.name}
+										tab={tab}
+										handleClick={() => { }}
+									/>
+								))}
+							</div>
+						</div>
+					</motion.div>
+
+					<motion.div
+						className='absolute z-10 top-5 right-5'
+						{...fadeAnimation}
+					>
+
+					</motion.div>
+				</>
+			)}
+		</AnimatePresence>
 	)
 }
 
